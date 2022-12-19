0feb797dcf2b02b4a4f74e3e0f590ac3a0fb9182
Prathamesh Sarjerao Vaidya <82259786+prathameshv07@users.noreply.github.com>
2022-12-19 18:56:43 +0530

    Update server.c

--- server.c
+++ server.c
@@ -1,91 +1,45 @@
-#include <rpc/rpc.h>
-#include "calculator.h"
-#include <math.h>
-
-/*Version 1: Un-rounded result*/
-//addition
-float * add_1_svc(numbers *argp, struct svc_req *rqstp)
+#include "calculate.h"
+ 
+float * add_1_svc(inputs *argp, struct svc_req *rqstp)
 {
-	static float result;
-	printf("add(%f, %f) is called\n", argp->a, argp->b);
-	result = argp->a+argp->b;
-	return &result;
+ static float result;
+ 
+ result = argp->num1+argp->num2;
+ printf("Got Request : Adding %f and %f\n",argp->num1,argp->num2);
+ printf("Sent Response : %f\n",result);
+ 
+ return &result;
 }
-
-//subtraction
-float * sub_1_svc(numbers *argp, struct svc_req *rqstp)
+ 
+float * sub_1_svc(inputs *argp, struct svc_req *rqstp)
 {
-	static float result;
-	printf("sub(%f, %f) is called\n", argp->a, argp->b);
-	result = argp->a-argp->b;
-	return &result;
+ static float result;
+ 
+ result = argp->num1-argp->num2;
+ printf("Got Request : substituting %f from %f\n",argp->num2,argp->num1);
+ printf("Sent Response : %f\n",result);
+ 
+ return &result;
 }
-
-//multiplication
-float * mul_1_svc(numbers *argp, struct svc_req *rqstp)
+ 
+float * mul_1_svc(inputs *argp, struct svc_req *rqstp)
 {
-	static float result;
-	printf("mul(%f, %f) is called\n", argp->a, argp->b);
-	result = argp->a*argp->b;
-	return &result;
+ static float result;
+ 
+ result = argp->num1*argp->num2;
+ printf("Got Request : Multiplying %f by %f\n",argp->num1,argp->num2);
+ printf("Sent Response : %f\n",result);
+ 
+ return &result;
 }
-
-//division
-float * div_1_svc(numbers *argp, struct svc_req *rqstp)
+ 
+float * div_1_svc(inputs *argp, struct svc_req *rqstp)
 {
-	static float result;
-	printf("div(%f, %f) is called\n", argp->a, argp->b);
-	result = argp->a/argp->b;
-	return &result;
+ static float result;
+ 
+ result = argp->num1/argp->num2;
+ printf("Got Request : Dividing %f by %f\n",argp->num1,argp->num2);
+ printf("Sent Response : %f\n",result);
+ 
+ return &result;
 }
-
-
-
-
-/*Version 2: The rounded result*/
-//addition
-float * add_2_svc(numbers1 *argp, struct svc_req *rqstp)
-{
-	static float result;
-	result = result + 0.5;
-	result = (int)result;
-	printf("add(%f, %f) is called\n", argp->aa, argp->bb);
-	result = argp->aa+argp->bb;
-	return &result;
-}
-
-//subtraction
-float * sub_2_svc(numbers1 *argp, struct svc_req *rqstp)
-{
-	static float result;
-	result = result + 0.5;
-	result = (int)result;
-	printf("sub(%f, %f) is called\n", argp->aa, argp->bb);
-	result = argp->aa-argp->bb;
-	return &result;
-}
-
-//multiplication
-float * mul_2_svc(numbers1 *argp, struct svc_req *rqstp)
-{
-	static float result;
-	result = result + 0.5;
-	result = (int)result;
-	printf("mul(%f, %f) is called\n", argp->aa, argp->bb);
-	result = argp->aa*argp->bb;
-	return &result;
-}
-
-//division
-float * div_2_svc(numbers1 *argp, struct svc_req *rqstp)
-{
-	static float result;
-	result = result + 0.5;
-	result = (int)result;
-	printf("div(%f, %f) is called\n", argp->aa, argp->bb);
-	result = argp->aa/argp->bb;
-	return &result;
-}
-
-
-
